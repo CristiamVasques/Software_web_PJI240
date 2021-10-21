96ddc0786b9242eba60584277c3d6e1322845675
Jeffersonn11 <jeffersonn.souza35@gmail.com>
2021-10-20 21:56:51 -0300

    Criando a tabela do projeto

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from core.models import Servico
 
 # Register your models here.
+
+class ServicoAdmin(admin.ModelAdmin):
+    list_display = ('servico', 'data_criacao')
+    list_filter = ('nome_completo',)
+
+admin.site.register(Servico, ServicoAdmin)
+
--- core/models.py
+++ core/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Servico(models.Model):
+    nome_completo = models.CharField(max_length=50, null=True)
+    empresa = models.CharField(max_length=200)
+    telefone = models.CharField(max_length=15)
+    email = models.CharField(max_length=100)
+    prioridade = models.CharField(max_length=50)
+    servico = models.CharField(max_length=50)
+    descricao = models.TextField(blank=True, null=True)
+    data_criacao= models.DateTimeField(auto_now=True, verbose_name='Solicitado')
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'servico'
+
+    def __str__(self):
+        return self.servico
+
+
+
+
